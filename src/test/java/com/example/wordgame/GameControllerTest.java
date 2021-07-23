8b4416938a89395ab69ec58af881f1307eecb782
GtName <girsammatvienko@gmail.com>
2021-07-23 14:44:25 +0300

    completed

--- src/test/java/com/example/wordgame/GameControllerTest.java
+++ src/test/java/com/example/wordgame/GameControllerTest.java
@@ -4,6 +4,7 @@ import com.example.wordgame.controllers.GameController;
 import com.example.wordgame.entitities.Game;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -11,16 +12,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -35,13 +37,67 @@ public class GameControllerTest {
         Game game = new Game();
         game.setWords(toList(new String[] {"fish", "horse", "egg", "goose", "eagle"}));
         Game resultGame = new Game();
-        game.setWords(toList(new String[] {"fish", "horse", "egg", "goose", "eagle"}));
-        mockMvc.perform(MockMvcRequestBuilders
+        resultGame.setWords(toList(new String[] {"fish", "horse", "egg", "goose", "eagle"}));
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders
                 .post("http://localhost:8080/api/play/").content(objectMapper.writeValueAsString(game))
-                .contentType(MediaType.APPLICATION_JSON)
-        )
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.*").value(objectMapper.writeValueAsString(resultGame)));
+                .contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        String outputWords = result.getResponse().getContentAsString();
+        String expectedOutputWords = objectMapper.writeValueAsString(resultGame);
+        Assert.assertEquals(outputWords, expectedOutputWords);
+    }
+
+    @Test
+    public void testCase2() throws Exception {
+        Game game = new Game();
+        game.setWords(toList(new String[] {"fish", "horse", "snake", "goose", "eagle"}));
+        Game resultGame = new Game();
+        resultGame.setWords(toList(new String[] {"fish", "horse"}));
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders
+                .post("http://localhost:8080/api/play/").content(objectMapper.writeValueAsString(game))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        String outputWords = result.getResponse().getContentAsString();
+        String expectedOutputWords = objectMapper.writeValueAsString(resultGame);
+        Assert.assertEquals(outputWords, expectedOutputWords);
+    }
+
+    @Test
+    public void testCase3() throws Exception {
+        Game game = new Game();
+        game.setWords(toList(new String[] {"fish", "horse", "", "goose", "eagle"}));
+        Game resultGame = new Game();
+        resultGame.setWords(toList(new String[] {"fish", "horse"}));
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders
+                .post("http://localhost:8080/api/play/").content(objectMapper.writeValueAsString(game))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        String outputWords = result.getResponse().getContentAsString();
+        String expectedOutputWords = objectMapper.writeValueAsString(resultGame);
+        Assert.assertEquals(outputWords, expectedOutputWords);
+    }
+
+    @Test
+    public void testCase4() throws Exception {
+        Game game = new Game();
+        game.setWords(toList(new String[] {"", "horse", "", "goose", "eagle"}));
+        Game resultGame = new Game();
+        resultGame.setWords(toList(new String[] {}));
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders
+                .post("http://localhost:8080/api/play/").content(objectMapper.writeValueAsString(game))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        String outputWords = result.getResponse().getContentAsString();
+        String expectedOutputWords = objectMapper.writeValueAsString(resultGame);
+        Assert.assertEquals(outputWords, expectedOutputWords);
     }
 
     private static List<String> toList(String[] string) {
@@ -51,4 +107,6 @@ public class GameControllerTest {
         }
         return result;
     }
+
+
 }
